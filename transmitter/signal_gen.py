dd5dcef92408e75eef52b73cab9b000474592bbd
xubyaj <26120621+xubyaj@users.noreply.github.com>
2025-07-16 22:14:23 -0500

    updated signal gen code to change time

--- transmitter/signal_gen.py
+++ transmitter/signal_gen.py
@@ -14,7 +14,7 @@ def print_progress_bar(iteration, total, length=40):
 
 # Parameters
 fs = 100  # Sampling frequency in Hz
-duration_minutes = 5  # Duration in minutes
+duration_minutes = 50  # Duration in minutes
 duration_seconds = duration_minutes * 60  # Convert minutes to seconds
 
 # Time vector
